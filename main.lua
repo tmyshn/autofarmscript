0267ddaf4bc06e0d0a2dfb58b91b934c76520c34
tmyshn <tmyshn33@gmail.com>
2022-12-19 05:10:36 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -77,12 +77,12 @@ end
 BlurEffect:Destroy()
 ScreenGui:Destroy()
 
---[[local queue_on_teleport =
+local queue_on_teleport =
     queue_on_teleport or
     syn and
         syn.queue_on_teleport [[
        repeat wait() until game:IsLoaded() wait(50) print("ServerHoped or rejoined")
-       loadstring(game:HttpGet('https://raw.githubusercontent.com/tmyshn/autofarmscript/main/main.lua'))()]]   --]]
+       loadstring(game:HttpGet('https://raw.githubusercontent.com/tmyshn/autofarmscript/main/main.lua'))()]]   -
 
        --[[
        local queueteleport =fluxus and fluxus.queue_on_teleport
