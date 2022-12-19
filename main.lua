5af0e140b5659767280428225358849dfda9a881
tmyshn <tmyshn33@gmail.com>
2022-12-19 04:46:52 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -85,7 +85,7 @@ ScreenGui:Destroy()
        loadstring(game:HttpGet('https://raw.githubusercontent.com/tmyshn/autofarmscript/main/main.lua'))()]]   --]]
 
        local queueteleport =fluxus and fluxus.queue_on_teleport
-queueteleport("repeat wait() until game:IsLoaded() wait(50) print('ServerHoped or rejoined')loadstring(game:HttpGet('https://raw.githubusercontent.com/tmyshn/autofarmscript/main/main.lua'))()")
+queueteleport("repeat wait() until game:IsLoaded() wait(50) loadstring(game:HttpGet('https://raw.githubusercontent.com/tmyshn/autofarmscript/main/main.lua'))()")
 
 
 
