fe4895c5a40ece4869a2784d6c572f0ee060ec05
tmyshn <tmyshn33@gmail.com>
2023-01-29 04:56:50 +0300

    Update icerik.lua

--- icerik.lua
+++ icerik.lua
@@ -449,12 +449,7 @@ local PetID
 local Pet
 local PetsShow = {}
 local List = {}
-for i, v in pairs(require(ReplicatedStorage.ClientModules.Core.ClientData).get_data()[Player.Name].inventory.pets) do
-    local Key = tostring(v["id"]) .. " - " .. tostring(v["properties"]["age"]) .. " years old"
-    PetsShow[Key] = v
-    table.insert(List, Key)
-    table.sort(List, key)
-end
+
 
 UserInputService.WindowFocusReleased:Connect(
     function()
